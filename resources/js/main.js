0d21d4120c0160004e60afaa103c2436de408f34
jennbrewr <jenngbrewer@gmail.com>
2018-02-04 21:06:27 -0500

    Added variables to subscription and my orders page

--- resources/js/main.js
+++ resources/js/main.js
@@ -24144,6 +24144,13 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 
 })();
 
+/* product dropdown: sort */
+$(document).on('change', 'select[data-sort-redirect]', function(){
+    path = window.location.pathname + '?' + $(this).val(),
+    baseUrl = window.location.protocol + '//' + window.location.host;
+    window.location = baseUrl + path;
+});
+
 
 /***/ })
 /******/ ]);
